866e0e04b156267b82508c32ed0bf56ddf3bc337
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-12-01 18:24:14 +0100

    Create Day1Part2.java

---
+++ Java/Day1Part2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aoc2019day1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author inquis1t0r
+ */
+public class AoC2019Day1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        String fileName = "C:\\Users\\inquis1t0r\\Documents\\NetBeansProjects\\AoC2019Day1\\src\\aoc2019day1\\input.txt";
+        
+        List<Integer> ints = Files.lines(Paths.get(fileName))
+                          .map(Integer::parseInt)
+                          .collect(Collectors.toList());
+        
+        int result = 0;
+        
+        for(int x : ints){
+        //System.out.println(x);
+        x = x / 3 - 2;
+            while(x > 0)
+            {
+                result += x;
+                x = x / 3 - 2;
+            }
+            
+        }
+        System.out.println(result);
+    }
+    
+}
