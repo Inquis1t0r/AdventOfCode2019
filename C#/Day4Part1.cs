4d57144d8a14bd12918fabc2d07dbf9154ed2faa
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-12-04 08:52:43 +0100

    Create Day4Part1.cs

---
+++ C#/Day4Part1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+
+
+namespace AoC2019Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            foreach (int password in Enumerable.Range(234208, 765869 - 234208))
+            {
+                int pswdNum = password;
+
+                bool isAscending = true;
+                bool hasDuplicate = false;
+                var last = 0;
+
+                while (pswdNum != 0)
+                {
+                    last = pswdNum % 10;
+                    pswdNum = pswdNum / 10;
+
+                    if (pswdNum % 10 > last)
+                    {
+                        isAscending = false;
+                        break;
+                    }
+
+                    if (pswdNum % 10 == last)
+                    {
+                        hasDuplicate = true;
+                    }
+                }
+
+                if (isAscending && hasDuplicate)
+                {
+                    result += 1;
+                }
+                    
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+}
